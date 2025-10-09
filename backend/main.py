e02e999ce17b9559b9f0cd8a26c35b3f19303bb2
Egor Aksenov <anonaso3011@gmail.com>
2025-10-09 18:00:43 +0300

    test fix

--- backend/main.py
+++ backend/main.py
@@ -3,6 +3,9 @@ import telebot
 from fastapi import FastAPI, HTTPException
 import random
 
+import logging
+from colorlog import ColoredFormatter
+
 import datetime
 
 #pip install fastapi uvicorn pydantic pytelegrambotapi
@@ -12,7 +15,41 @@ from pydantic import BaseModel
 app = FastAPI()
 bot = telebot.TeleBot("8407018520:AAGWhWY8yK-0lgYoeShAAN_oLUItqa-k8RY")
 
-chat_id_admin = "fdsfdsfsdf"
+
+logger = logging.getLogger('backend')
+logger.setLevel(logging.DEBUG)
+
+
+file_handler = logging.FileHandler('app.log', 'a', encoding='utf-8')
+file_handler.setLevel(logging.ERROR)
+
+log_format = logging.Formatter('%(asctime)s  -  %(levelname)s:    %(message)s')
+
+file_handler.setFormatter(log_format)
+
+logger.addHandler(file_handler)
+
+# Создаем обработчик (например, консольный)
+handler = logging.StreamHandler()
+
+    # Создаем форматтер с цветами
+formatter = ColoredFormatter(
+        '%(log_color)s%(levelname)s:     %(asctime)s -  %(name)s   -   %(message)s',
+        log_colors={
+            'DEBUG': 'cyan',
+            'INFO': 'green',
+            'WARNING': 'yellow',
+            'ERROR': 'red',
+            'CRITICAL': 'bold_red',
+        }
+    )
+
+    # Добавляем форматтер к обработчику
+handler.setFormatter(formatter)
+
+logger.addHandler(handler)
+
+chat_id_admin = ""
 
 class CheckTransfer(BaseModel):
     bank: str
@@ -27,12 +64,16 @@ class CheckTransfer(BaseModel):
 @app.get("/new_user")
 def startMes():
     try:
+        logger.info('Пользователь зашел на сайт')
         bot.send_message(
             chat_id=chat_id_admin,
             text=f"🔐 Зашел новый пользователь \n{datetime.datetime.now()}"
         )
+        logger.info('Данные отправленны администратору')
         return {"Status": "good"}
     except Exception as ex:
+        logger.error(ex)
+
         raise HTTPException(status_code=400, detail="Server cant send message")
 
 
@@ -49,3 +90,8 @@ def startMes(data: CheckTransfer):
         return {"data": id}
     except Exception as ex:
         raise HTTPException(status_code=400, detail="Server cant send message")
+    
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
